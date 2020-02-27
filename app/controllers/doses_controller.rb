004b48824def0d4ef3202932230b04b2fc5208f0
Noemi Heusler <nlheusler@gmail.com>
2020-02-27 07:43:02 +0100

    controllers added

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,36 @@
+class DosesController < ApplicationController
+  def index
+
+  end
+
+  def show
+  end
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'doses/index'
+  get 'doses/show'
+  get 'doses/new'
+  get 'doses/create'
+  get 'doses/destroy'
+  resources :cocktails, only: [:index, :show, :new, :create] do
+    resources :doses, only: [:index, :new, :create]
+  end
+  resources :doses, only: [:show, :destroy]
 end
