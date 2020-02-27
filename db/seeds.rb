feca03b1d6f21316d0936a7a49da62218330975b
Noemi Heusler <nlheusler@gmail.com>
2020-02-27 16:04:43 +0100

    more seed data and navbar

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,6 +6,52 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-Ingredient.create(name: "lemon")
-Ingredient.create(name: "ice")
-Ingredient.create(name: "mint leaves")
+require 'open-uri'
+require 'json'
+
+url = "https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list"
+
+response = open(url).read
+repo = JSON.parse(response)
+
+ingredients = repo["drinks"]
+
+ingredients.each do |ingredient|
+  new_ingredient = Ingredient.new(name: ingredient["strIngredient1"])
+  new_ingredient.save!
+end
+
+("a".."z").each do |letter|
+url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?f=#{letter}"
+puts "getting #{letter}"
+response = open(url).read
+cocktail_repo = JSON.parse(response)
+
+cocktails = cocktail_repo["drinks"]
+
+# p cocktails.first['strGlass']
+next if cocktails.nil?
+cocktails.each do |cocktail|
+  new_cocktail = Cocktail.create(name: cocktail['strDrink'])
+  i = 1
+  loop do
+    ingredient_name = cocktail["strIngredient#{i}"]
+    ingredient_description = cocktail["strMeasure#{i}"]
+    break if ingredient_name == nil
+
+    new_ingredient = Ingredient.find_or_create_by(name: ingredient_name)
+    Dose.create(cocktail: new_cocktail, ingredient: new_ingredient, description: ingredient_description)
+    i += 1
+    end
+  end
+    # ingredient = Ingredient.find_by(name: ingredient_name)
+    # if ingredient
+    #   Dose.create(cocktail: new_cocktail, ingredient: ingredient, description: ingredient_description)
+    # else
+    #   new_ingredient = Ingredient.create(name: ingredient_name)
+    #   Dose.create(cocktail: new_cocktail, ingredient: new_ingredient, description: ingredient_description)
+    # end
+
+  # p cocktail['strGlass']
+
+end
