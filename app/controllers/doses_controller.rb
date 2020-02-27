e39431f6edec18e1abe27e108e4efbb699449b55
Noemi Heusler <nlheusler@gmail.com>
2020-02-27 12:09:18 +0100

    mr cocktail design mvp

--- app/controllers/doses_controller.rb
+++ app/controllers/doses_controller.rb
@@ -1,6 +1,6 @@
 class DosesController < ApplicationController
   def index
-
+    @doses = Dose.all
   end
 
   def show
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,4 @@
 Rails.application.routes.draw do
-  get 'doses/index'
-  get 'doses/show'
-  get 'doses/new'
-  get 'doses/create'
-  get 'doses/destroy'
   resources :cocktails, only: [:index, :show, :new, :create] do
     resources :doses, only: [:index, :new, :create]
   end
